3ee9ac2f7595060bdf016c59a91516c9ae39f4b2
June Cho <chesefo@gmail.com>
2021-02-07 16:45:50 +0900

    Initial Commit

---
+++ Shared/BarimWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarimWalletApp.swift
+//  Shared
+//
+//  Created by Junhyung Cho on 2021/02/07.
+//
+
+import SwiftUI
+
+@main
+struct BarimWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
